77ac3a8afbd1a4e3e430e3f7e239f2bf2e7f4362
VinayakVarsani <vinayak.varsani@bacancy.com>
2022-04-11 19:34:40 +0530

    CRUD updates

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+    root "articles#index"
+
+    resources :articles
+  
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  
+
+end
